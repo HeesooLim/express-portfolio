50512eb56c4f0f939d4142fc12525e9cd4d4df03
heesoolim <hlim29@my.centennialcollege.ca>
2020-10-04 14:35:16 -0400

    added services content and contact content

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,6 +4,23 @@
     function Start()
     {
         console.log("app started");
+        formValidate();
+    }
+    
+    function formValidate()
+    {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        let forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        let validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+        });
     }
 
     window.addEventListener("load", Start);
